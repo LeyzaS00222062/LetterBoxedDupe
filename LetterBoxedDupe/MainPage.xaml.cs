69a99ebebf897a65b2a02464a7565307a5ebbe8d
Leyzanelle Pedregosa <S00222062@atu.ie>
2025-03-24 20:22:16 +0000

    Adding functions to MainPage.cs

--- LetterBoxedDupe/MainPage.xaml.cs
+++ LetterBoxedDupe/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using static System.Net.WebRequestMethods;
+﻿using Microsoft.UI.Xaml.Media.Animation;
+using System.Collections.ObjectModel;
+using Windows.ApplicationModel.Search;
+using static System.Net.WebRequestMethods;
 
 namespace LetterBoxedDupe
 {
@@ -7,17 +10,30 @@ namespace LetterBoxedDupe
         private const string API_KEY = "ec13022";
         private const string API_URL = "https://www.omdbapi.com/";
 
-        private Movie _movie; 
+        public ObservableCollection<Movie> Movies { get; set; } = new ObservableCollection<Movie>();
 
-        public MainPage(Movie movie)
+        public MainPage()
         {
             InitializeComponent();
-            _movie = movie;
-            LoadMovieDetails();
+            BindingContext = this;
         }
 
-        
+        private async void OnSearchMovies(object sender, EventArgs e)
+        {
+            string searchMovie = searchBarMovie.Text;
+            if (string.IsNullOrEmpty(searchMovie))
+            {
+               
 
+            }
+
+
+        }
+        private async Task FetchMoviesFromApi(string query)
+        {
+            using HttpClient client = new HttpClient();
+
+        }
         
         
     }
