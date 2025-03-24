5092d18c470f22146ba50a3b468487a807939a26
Lj <Lj@DESKTOP-JSN10UI>
2025-03-24 17:18:03 +0000

    Add project files.

---
+++ LetterBoxedDupe/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LetterBoxedDupe
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LetterBoxedDupe/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LetterBoxedDupe
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
